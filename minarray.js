4f8c5469d2d910288dbf6b627db41be66e5a4757
Krishna Reddy <122253829+krishnareddy9390@users.noreply.github.com>
2023-01-26 13:13:37 +0530

    Update minarray.js

--- minarray.js
+++ minarray.js
@@ -1,95 +1,39 @@
-// function myfunction1(){
-//     var arr = ['d','e','f','a','i'];
-//     var small =arr[0];
-//     for(i = 1 ; i < arr.length; i++){
-//         if(arr[i] < small){
-//             console.log(arr[i]);
-//                 // small = arr[i];
-//             }
-//     }
-//     console.log(small);
-
-   
-// }
-// myfunction1();
-// if(isNaN(arr[i]))
-// {
-//     if(arr[i] < smallCHaracter){
-//         smallCHaracter = arr[i]
-//     }
-// }
-// }
-// var tempArr = []
-// 
-
-
-
-
-
-
-
-
-var arr=['krishna','85785','ashok','65875','javid','95496'];
-var int= [];
-var strng = [];
-
-  for(i=0;i<arr.length;i++){
-        if(parseInt(arr[i])){
-            int.push(arr[i]);
-        }else{
-            strng.push(arr[i]);
-        }
-    
- }
- // to print the interger
- console.log(int);
-// to display the smallest integer
-var Number=[];
- function myfunction3(){
-    var Small = int[0];
-    for(j=1 ;j < int.length  ;j++){
-        if(int[j] < Small){
-            Small=int[j];
-        }
+function validate(){
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    if(username.value =="" && password.value==""){
+        text = "Enter valid username and password";
+      
+      document.getElementById("error1").innerHTML = text;
+      document.getElementById("error").innerHTML = "";
+      document.getElementById("errors").innerHTML = "";
+        return false;
     }
-    // console.log("Smalllest Number: "+Small);
-    Number.push(Small);
-    console.log(Number);
-}
-myfunction3();
-
-
-// to print the strings
-console.log(strng);
-
-// to display the smaalest string
-var lest =[];
-function myfunction4(){
-    var SMALL =strng[0];
-   
-    for(k=1;k < strng.length ; k++){
-        if(strng[k] < SMALL){
-            SMALL = strng[k];
-        }
+    else{
+        document.getElementById("error1").innerHTML = "";
+  
     }
-    //  console.log("Smallest strng  is :"+ SMALL);
-     lest.push(SMALL);
-     console.log(lest);
-     
-}
-myfunction4();
+    if(password.value=="" || username.value >1){
+        password="Enter valid password"
+          document.getElementById("errors").innerHTML = password;
+          document.getElementById("error").innerHTML = "";
 
-
-
-
-
-// function myfunction5(){
-    // var empty=[];
-    // empty.push(lest);
-    // empty.push(Number);
-    // console.log(empty);
+    return false;
+       }
     
-// }
-// myfunction5();
-var total=Number.concat(lest);
-    console.log(total);  
+    else{
+        document.getElementById("errors").innerHTML = "";
+    }
+   if(username.value=="" || password.value>1){
+    username="enter the valid name"
+    document.getElementById("error").innerHTML = username;
+    document.getElementById("errors").innerHTML = "";
+
+return false;
+   }
+   else{
+    document.getElementById("error").innerHTML = "";
+
+   }
+   
+}
